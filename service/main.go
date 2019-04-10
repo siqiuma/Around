67b581acd25ab80ce74c2419bfa48f672ea16155
siqiuma <siqiuma@gmail.com>
2019-04-09 20:43:03 -0400

    third commit

--- service/main.go
+++ service/main.go
@@ -245,7 +245,7 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
     saveToES(p, id)
 
     // Save to BigTable.
-    //saveToBigTable(p, id)
+    saveToBigTable(p, id)
 
 }
 
@@ -318,4 +318,30 @@ func containsFilteredWords(s *string) bool {
         }
     }
     return false
+}
+
+func saveToBigTable(p *Post, id string) {
+    ctx := context.Background()
+    // you must update project name here
+    bt_client, err := bigtable.NewClient(ctx, PROJECT_ID, BT_INSTANCE)
+    if err != nil {
+        panic(err)
+        return
+    }
+
+    tbl := bt_client.Open("post")
+    mut := bigtable.NewMutation()
+    t := bigtable.Now()
+
+    mut.Set("post", "user", t, []byte(p.User))
+    mut.Set("post", "message", t, []byte(p.Message))
+    mut.Set("location", "lat", t, []byte(strconv.FormatFloat(p.Location.Lat, 'f', -1, 64)))
+    mut.Set("location", "lon", t, []byte(strconv.FormatFloat(p.Location.Lon, 'f', -1, 64)))
+
+    err = tbl.Apply(ctx, id, mut)
+    if err != nil {
+        panic(err)
+        return
+    }
+    fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
 }
